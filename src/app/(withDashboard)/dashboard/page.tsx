2972fba9c8725db54b9b16b94ecebbf539830b12
Mehebul02 <mehebul2123@gmail.com>
2025-03-10 21:29:30 +0600

    Added the github session

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -8,15 +8,16 @@ const DashboardPage = async() => {
     <div>
 
     {
-      session?.user &&
+      session?.user ?
        <>
        <h1 className="text-4xl text-center mt-10">Welcome To {session?.user?.name}</h1>
         <h1 className="text-4xl text-center mt-10">Welcome To {session?.user?.email}</h1>
-        <Image src={session?.user?.image} height={400} width={400} alt="user image"/>
-       </>
+        <Image className="rounded-full mx-auto my-10 border-2 border-emerald-700"  src={session?.user?.image} height={100} width={100} alt="user image"/>
+       </>:
+       <h1 className="text-center mt-20 text-3xl">Next Authentication</h1>
     }
 
-    <h1>User Pending</h1>
+   
     </div>
   );
 };

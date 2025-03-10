9d41e71e7f877b6d6b43c7956b3a86aa015bd300
Mehebul02 <mehebul2123@gmail.com>
2025-03-10 22:23:27 +0600

    Added the authnication

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -94,7 +94,9 @@ const LoginPage = () => {
 
           {/* Social Login Buttons */}
           <div className="flex justify-center gap-4 mt-4">
-            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
+            <button onClick={()=>signIn('google',{
+              callbackUrl:'http://localhost:3000/dashboard'
+            })} className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={30}
@@ -103,7 +105,7 @@ const LoginPage = () => {
               />
             </button>
             <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200" onClick={()=>signIn("github",{
-              callbackUrl:"http://localhost:300/dashboard"
+              callbackUrl:"http://localhost:3000/dashboard"
             })}>
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"

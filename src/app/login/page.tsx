4c60ad020d016350faf829b5cc7fded07c4fa3d9
Mehebul02 <mehebul2123@gmail.com>
2025-03-11 00:14:27 +0600

    Added the register and login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
-import {  signIn } from "next-auth/react"
-type FormValues = {
+import { signIn } from "next-auth/react"
+import { loginUser } from "@/utils/actions/loginUser";
+import { useRouter } from "next/navigation";
+export type FormValues = {
   email: string;
   password: string;
 };
@@ -15,8 +17,21 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FormValues>();
 
+  const route = useRouter()
+
   const onSubmit = async (data: FormValues) => {
-    console.log(data);
+    try {
+      const res = await loginUser(data)
+      if (res.accessToken) {
+        alert(res.message)
+        localStorage.setItem('access token', res.accessToken)
+        route.push('/')
+      }
+      console.log(res);
+    } catch (err: any) {
+      console.error(err.message);
+      throw new Error(err.message);
+    }
   };
 
   return (
@@ -94,8 +109,8 @@ const LoginPage = () => {
 
           {/* Social Login Buttons */}
           <div className="flex justify-center gap-4 mt-4">
-            <button onClick={()=>signIn('google',{
-              callbackUrl:'http://localhost:3000/dashboard'
+            <button onClick={() => signIn('google', {
+              callbackUrl: 'http://localhost:3000/dashboard'
             })} className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
@@ -104,8 +119,8 @@ const LoginPage = () => {
                 alt="Google logo"
               />
             </button>
-            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200" onClick={()=>signIn("github",{
-              callbackUrl:"http://localhost:3000/dashboard"
+            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200" onClick={() => signIn("github", {
+              callbackUrl: "http://localhost:3000/dashboard"
             })}>
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
